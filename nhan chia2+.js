4414ed687ceff2f89407652218f8cc5e18461a34
Quanninanda <hoangminhquan2001x@gmail.com>
2023-05-16 11:21:09 +0700

    gg

---
+++ nhan chia2+.js
@@ -0,0 +1,14 @@
+function nhan2(){
+    let a = document.getElementById("input")
+    do {
+        let kq = a.value * 2;
+        a.value=kq;
+    }while (kq!=0)
+}
+function chia2(){
+    let a = document.getElementById("input")
+    do {
+        let kq = a.value / 2;
+        a.value=kq;
+    }while (kq!=0)
+}
